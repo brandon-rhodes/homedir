a3ad0a07032c55d98c4d746bae9b634a685e3799
Brandon Rhodes <brandon@rhodesmill.org>
2015-09-05 12:48:06 -0400

    Remove "chairing" as it deserves its own repo

--- usr/chairing/run1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm -f out.json && scrapy runspider -o out.json -t json spider_django_cms.py
--- usr/chairing/run2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#jq 'map(.body |= . / "\r\n")' out.json
-jq 'sort_by(.page_id)' out.json
