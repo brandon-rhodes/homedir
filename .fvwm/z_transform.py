cc6b702da2a87d68d7b6446aa35f1d68539b5844
Brandon Rhodes <brandon@rhodesmill.org>
2017-10-20 12:31:19 -0400

    Tweak DPI and clipboard selection of Win-Z

--- .fvwm/z_transform.py
+++ .fvwm/z_transform.py
@@ -5,7 +5,7 @@
 from subprocess import PIPE, Popen, check_output
 
 def main():
-    content = check_output(['xclip', '-o'])
+    content = check_output(['xclip', '-o', '-selection', 'clipboard'])
     lcontent = content.lstrip()
     print repr(content)
     if lcontent.startswith('D') and lcontent[1:].isdigit():
@@ -20,7 +20,7 @@ def main():
         with open('/tmp/tmp.tex', 'w') as f:
             f.write('\\nopagenumbers\n' + content + '\n\\end\n')
         check_output(['tex', 'tmp.tex'], cwd='/tmp')
-        check_output(['dvipng', '-D', '300', 'tmp.dvi'], cwd='/tmp')
+        check_output(['dvipng', '-D', '150', 'tmp.dvi'], cwd='/tmp')
         check_output(['convert', '-trim', 'tmp1.png', 'tmp2.png'], cwd='/tmp')
         with open('/tmp/tmp2.png') as f:
             data = f.read()
