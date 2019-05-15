f92906428e2a413212e6c978a447df6b1c5a097a
Brandon Rhodes <brhodes@dropbox.com>
2019-05-15 11:30:23 -0400

    Have Pastes menu update both paste buffers
    It seems that autocutsel doesn’t mirror between paste buffers when I
    write them with xclip, so I’ll write both buffers myself instead.

--- .fvwm/paste-menu.sh
+++ .fvwm/paste-menu.sh
@@ -6,7 +6,7 @@ for path in ~/.fvwm/pastes/*
 do
     name="$(basename $path)"
     letter="${name:0:1}"
-    echo '+ "&'"$letter"'. '"$name"'" Exec xclip -selection clipboard '"$path"
+    echo '+ "&'"$letter"'. '"$name"'" Exec xclip -selection clipboard '"$path"' && xclip -selection primary '"$path"
 done
 cat <<'EOF'
 + "&Markdown" Exec xclip -out -selection clipboard | pandoc | xclip -selection clipboard -target text/html
