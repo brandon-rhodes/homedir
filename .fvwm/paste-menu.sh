027c73a42f062be9bb68d88a76fe659b1cdde510
Brandon Rhodes <brandon@rhodesmill.org>
2020-09-03 08:04:37 -0400

    Remove old Markdown item from menu
    This will free up “M” to unambiguously be the shortcut for “matplotlib”,
    and stop duplicating functionality that’s now in the Transforms menu.

--- .fvwm/paste-menu.sh
+++ .fvwm/paste-menu.sh
@@ -9,6 +9,5 @@ do
     echo '+ "&'"$letter"'. '"$name"'" Exec xclip -selection clipboard '"$path"' && xclip -selection primary '"$path"
 done
 cat <<'EOF'
-+ "&Markdown" Exec xclip -out -selection clipboard | pandoc | xclip -selection clipboard -target text/html
 + DynamicPopUpAction PipeRead .fvwm/paste-menu.sh
 EOF
