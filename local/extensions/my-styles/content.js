24791e9fe3c88c05dcf35cc19db52588d0fa2c1f
Brandon Rhodes <brandon@rhodesmill.org>
2021-01-31 23:53:53 -0500

    Quick Chrome extension to fix a web site

---
+++ local/extensions/my-styles/content.js
@@ -0,0 +1,4 @@
+// var images = document.getElementsByTagName('img');
+// for (var i = 0, l = images.length; i < l; i++) {
+//   images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+// }
